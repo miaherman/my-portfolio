134918b65fb642ee01fa73523334003741fae235
miaherman <mia.herman@live.se>
2021-04-05 01:34:35 +0200

    projects

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import Ballaur from '../portfolio/Ballaur.png';
+import Kalender from '../portfolio/Kalender.png';
+
+export const myPortfolioImages = [
+        {
+            image: Ballaur,
+            name: 'Ballaur',
+        },
+        {
+            image: Kalender,
+            name: 'kalender',
+        }
+    ];
+
